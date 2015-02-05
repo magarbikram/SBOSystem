1ea2ec8aefbcb9d8f968522df8451202710e7480
Bikram <magar.bik@gmail.com>
2015-02-05 16:08:04 -0700

    added sales order form

---
+++ SBOSystemForm/SalesOrderForm.b1f.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAPbouiCOM.Framework;
+
+namespace SBOSystemForm
+{
+    [FormAttribute("139", "SalesOrderForm.b1f")]
+    class SalesOrderForm : SystemFormBase
+    {
+        public SalesOrderForm()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_0").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.Button Button0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+    }
+}
