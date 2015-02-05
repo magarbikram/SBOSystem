d11257c86f4a0a22a199ca26344a761e4611f16f
Bikram <magar.bik@gmail.com>
2015-02-05 14:32:04 -0700

    Added Item Master Form
    Initail Form Update

---
+++ SBOSystemForm/ItemMasterForm.b1f.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAPbouiCOM.Framework;
+
+namespace SBOSystemForm
+{
+    [FormAttribute("150", "ItemMasterForm.b1f")]
+    class ItemMasterForm : SystemFormBase
+    {
+        public ItemMasterForm()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_0").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.Button Button0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+    }
+}
